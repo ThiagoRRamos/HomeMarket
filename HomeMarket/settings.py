53876e5602dc694d98edf66e065d7c734ba14a04
Thiago R Ramos <thiagorramos@gmail.com>
2013-03-04 18:26:19 -0300

    Segundo commit

--- HomeMarket/settings.py
+++ HomeMarket/settings.py
@@ -1,7 +1,8 @@
 # Django settings for HomeMarket project.
 import dj_database_url
+import os
 
-
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -110,6 +111,7 @@ ROOT_URLCONF = 'HomeMarket.urls'
 WSGI_APPLICATION = 'HomeMarket.wsgi.application'
 
 TEMPLATE_DIRS = (
+    PROJECT_PATH + '/templates/'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -122,8 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
