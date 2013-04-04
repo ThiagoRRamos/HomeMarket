83490397e058207db973ec206c9b3c6b1a35e100
Thiago R Ramos <thiagorramos@gmail.com>
2013-04-04 00:56:48 -0300

    Adicionando ao cliente poder ver os produtos de determinado supermercado

--- HomeMarket/urls.py
+++ HomeMarket/urls.py
@@ -11,7 +11,9 @@ urlpatterns = patterns('',
     (r'^contas/', include('allauth.urls')),
     url(r'^criar/$', 'marketapp.views.supermercado.adicionar_produto'),
     url(r'^criar_produto', 'marketapp.views.supermercado.criar_produto'),
-    url(r'^adicionar-produto-existente/(?P<codigo>.*)', 'marketapp.views.supermercado.adicionar_produto_existente')
+    url(r'^adicionar-produto-existente/(?P<codigo>.*)',
+        'marketapp.views.supermercado.adicionar_produto_existente'),
+    url(r'^supermercado/(?P<nome>.*)$','marketapp.views.cliente.ver_produtos_supermercado')
 )
 
 if settings.DEBUG:
--- marketapp/views/cliente.py
+++ marketapp/views/cliente.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from marketapp.models import Supermercado, ProdutoSupermercado
+from django.http.response import Http404
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def ver_produtos_supermercado(request, nome):
+    try:
+        supermercado = Supermercado.objects.get(nome_url=nome)
+    except Supermercado.DoesNotExist:
+        raise Http404
+    produtos = ProdutoSupermercado.objects.filter(supermercado=supermercado)
+    return render(request,
+                  'ver_produtos_supermercado.html',
+                  {'produtos': produtos,
+                   'supermercado':supermercado})
