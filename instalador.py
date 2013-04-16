282047c604502d8309d827420dc547ed3b3ce9ab
Thiago R Ramos <thiagorramos@gmail.com>
2013-04-16 10:05:54 -0300

    Adicionando instalador

---
+++ instalador.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+
+dependencias = ["django",
+                "nose-exclude",
+                "selenium",
+                "nose",
+                "django-nose",
+                "requests",
+                "django-allauth"]
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomeMarket.settings")
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "instalar":
+            for dep in dependencias:
+                subprocess.call(["pip", "install", dep])
+    else:
+        for dep in dependencias:
+                subprocess.call(["pip", "install", dep])
