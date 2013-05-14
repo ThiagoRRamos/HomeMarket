f75cd3fbadfd4a1bd46ce19d05b2065e68bce87c
Lucas Amaral <lucasamaral1@gmail.com>
2013-05-14 02:24:07 -0300

    testa se usuário não logado, ou não supermercado consegue acessar a página

--- marketapp/tests/views/test_supermercado.py
+++ marketapp/tests/views/test_supermercado.py
@@ -3,13 +3,23 @@ from django.test.client import Client
 
 class TestSupermercado(TestCase):
     def setUp(self):
-    	super(TestSupermercado, self).setUp()
+        super(TestSupermercado, self).setUp()
         self.client = Client()
+
+    def tearDown(self):
+        super(TestSupermercado, self).tearDown()
+
+    def test_regiao_supermercado_logado(self):
         self.client.login(username='user-bretas', password='senha')
-
-	def tearDown(self):
-		super(TestSupermercado, self).tearDown()
-
-    def test_regiao(self):
         response = self.client.get('/definir-regiao/')
         self.assertEqual(response.status_code, 200)
+        self.client.logout()
+
+    def test_regiao_supermercado_nao_logado(self):
+        response = self.client.get('/definir-regiao/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_regiao_supermercado_nao_supermercado(self):
+    	self.client.login(username='cliente', password='senha')
+        response = self.client.get('/definir-regiao/')
+        self.assertEqual(response.status_code, 302)
