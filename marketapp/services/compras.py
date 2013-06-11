e3d4fa748c11541959e706ea0401e55c3f8c029c
Thiago R Ramos <thiagorramos@gmail.com>
2013-06-11 02:36:37 -0300

    Arrumando umas coisas

--- marketapp/services/compras.py
+++ marketapp/services/compras.py
@@ -3,8 +3,8 @@ Created on May 15, 2013
 
 @author: thiagorramos
 '''
-from marketapp.models import Compra, ProdutoCompra, CompraAgendada,\
-    ProdutoCompraAgendada
+from marketapp.models import Compra, ProdutoCompra, CompraAgendada, \
+    ProdutoCompraAgendada, CompraRecorrente, ProdutoCompraRecorrente
 import marketapp.services.regiao_atendimento as regiao_service
 
 
@@ -37,8 +37,19 @@ def gerar_compra_agendada(consumidor, produtos, data):
     return compra
 
 
-def gerar_compra_recorrente(consumidor, produtos):
-    pass
+def gerar_compra_recorrente(consumidor, produtos, frequencia):
+    supermercado = produtos[0].produto.supermercado
+    compra = CompraRecorrente.objects.create(modo_pagamento='nn',
+                                   status_pagamento='pn',
+                                   consumidor=consumidor,
+                                   supermercado=supermercado,
+                                   frequencia=frequencia)
+    for p in produtos:
+        ProdutoCompraRecorrente.objects.create(compra=compra,
+                                     produto=p.produto,
+                                     quantidade=p.quantidade,
+                                     preco_unitario=p.produto.preco)
+    return compra
 
 
 def custo_compras(supermercado, consumidor, produtosupermercados_map):
