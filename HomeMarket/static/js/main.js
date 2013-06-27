1257ae87be6172e42256f86422f854f3469a39c3
Thiago R Ramos <thiagorramos@gmail.com>
2013-06-27 09:53:06 -0300

    Arrumando questoes da lista

--- HomeMarket/static/js/main.js
+++ HomeMarket/static/js/main.js
@@ -1,5 +1,17 @@
 jQuery(function($){
-   $("#id_cpf").mask("999.999.999-99");
-   $("#id_cep").mask("99999-999");
-   $("#id_telefone").mask("(99)99999999?9");
+	if($("#id_cpf").length > 0){
+		$("#id_cpf").mask("999.999.999-99");
+	}
+	if($("#id_cep").length > 0){
+		$("#id_cep").mask("99999-999");
+	}
+	if($("#id_telefone").length > 0){
+		$("#id_telefone").mask("(99)99999999?9");
+	}
+	if($("input[id^=id_regiaoatendida_set-][id$=-cep_inicio]").length > 0){
+		$("input[id^=id_regiaoatendida_set-][id$=-cep_inicio]").mask("99999-999");
+	}
+	if($("input[id^=id_regiaoatendida_set-][id$=-cep_final]").length > 0){
+		$("input[id^=id_regiaoatendida_set-][id$=-cep_final]").mask("99999-999");
+	}
 });
